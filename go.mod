96739ef7d5dae24d81ba3e76e666ae0c952c1788
tattsun <dev.tattsun@gmail.com>
2022-08-06 20:50:09 +0900

    fix

--- go.mod
+++ go.mod
@@ -7,3 +7,8 @@ require (
 	github.com/quhar/bme280 v0.1.0
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
 )
+
+require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,123 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
+type Data struct {
+	Timestamp int64
+	Temp      float64
+	Press     float64
+	Hum       float64
+}
+
 func main() {
+	dbFilePath := os.Getenv("KNSKN_DB_FILEPATH")
+	if dbFilePath == "" {
+		dbFilePath = "./knskn.db"
+	}
+
 	bme, err := NewBME()
 	if err != nil {
 		log.Fatalf("failed to initialize BME: %+v", err)
 	}
 
-	temp, press, hum, err := bme.GetEnv()
+	db, err := leveldb.OpenFile(dbFilePath, nil)
 	if err != nil {
-		log.Fatalf("failed to get environments: %+v", err)
+		log.Fatalf("failed to open db: %+v", err)
 	}
+	defer db.Close()
 
-	fmt.Printf("Temp: %f\n", temp)
-	fmt.Printf("Pressure: %f\n", press)
-	fmt.Printf("Humidity: %f\n", hum)
+	go func() {
+		for {
+			temp, press, hum, err := bme.GetEnv()
+			if err != nil {
+				log.Printf("failed to get environments: %+v", err)
+			}
+
+			key := []byte(strconv.Itoa(int(time.Now().UnixMilli())))
+			value := []byte(fmt.Sprintf("%.2f,%.2f,%.2f", temp, press, hum))
+			if err := db.Put(key, value, nil); err != nil {
+				log.Printf("failed to put data to DB: %+v", err)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		from := r.URL.Query().Get("from")
+		if from == "" {
+			from = strconv.Itoa(int(time.Now().Add(-24 * time.Hour).UnixMilli()))
+		}
+
+		to := r.URL.Query().Get("to")
+		if to == "" {
+			to = strconv.Itoa(int(time.Now().UnixMilli()))
+		}
+
+		datum := []Data{}
+
+		iter := db.NewIterator(&util.Range{Start: []byte(from), Limit: []byte(to)}, nil)
+		defer iter.Release()
+		for iter.Next() {
+			timestampInt, err := strconv.Atoi(string(iter.Key()))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "unexpected error")
+				return
+			}
+
+			timestamp := int64(timestampInt)
+
+			parts := strings.Split(string(iter.Value()), ",")
+			temp, err := strconv.ParseFloat(parts[0], 64)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "unexpected error")
+				return
+			}
+
+			press, err := strconv.ParseFloat(parts[1], 64)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "unexpected error")
+				return
+			}
+
+			hum, err := strconv.ParseFloat(parts[2], 64)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w, "unexpected error")
+				return
+			}
+
+			datum = append(datum, Data{
+				Timestamp: timestamp,
+				Temp:      temp,
+				Press:     press,
+				Hum:       hum,
+			})
+		}
+		if err := iter.Error(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "unexpected error")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		enc := json.NewEncoder(w)
+		enc.Encode(datum)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
